33397062f30676db6d5bc21b3ac4b1c7752f343d
bcochran <b.cochrane@imperial.ac.uk>
2017-09-08 18:29:47 +0100

    update lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -23,7 +23,7 @@ def lambda_handler(event, context):
         print "Building Portfolio from " + str(location)
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-        portfolio_bucket = s3.Bucket('portfoliobuild.bryancochrane.net')
+        portfolio_bucket = s3.Bucket('portfolio.bryancochrane.net')
         build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
             for nm in myzip.namelist():
                 obj =  myzip.open(nm)
                 portfolio_bucket.upload_fileobj(obj, nm,
-                    ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm) [0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         print "Job Done!"
