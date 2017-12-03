0721a122c73a9cb49dfb0db266240863cff5345a
Bryan Cochrane <bcochran@bryans-imac.home>
2017-12-03 18:41:57 +0000

    modal

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -1,10 +1,69 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ExampleWork from '../js/example-work';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+import ExampleWork, { ExampleWorkBubble } from '../js/example-work'
+
+configure({ adapter: new Adapter() });
+
+const myWork = [
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Portfolio Boilerplate",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  }
+];
 
 describe("ExampleWork component", () => {
+  let component = shallow(<ExampleWork work={myWork}/>);
 
-  it("Should be a 'section' element", () => {
-    let component = shallow(<ExampleWork)
+  it("Should be a 'span' element", () => {
+    expect(component.type()).toEqual('span');
   });
+
+  it("Should contain as many children as there are work examples", () => {
+    expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
+  });
+
+  it("Should allow the modal to open and close", () => {
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
+  });
+
+});
+
+describe("ExampleWorkBubble component", () => {
+  let mockOpenModalFn = jest.fn();
+
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}
+    openModal={mockOpenModalFn}/>);
+
+  let images = component.find("img");
+
+  it("Should contain a single 'img' element", () => {
+    expect(images.length).toEqual(1);
+  });
+
+  it("Should have the image src set correctly", () => {
+    expect(images.getElement().props.src).toEqual(myWork[1].image.src);
+  });
+
+  it("Should call the openModal handler when clicked", () => {
+    component.find(".section__exampleWrapper").simulate('click');
+    expect(mockOpenModalFn).toHaveBeenCalled();
+  })
 });
