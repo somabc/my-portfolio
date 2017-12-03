613c57c924304607b3f129480d747c2219b14bf5
Bryan Cochrane <bcochran@bryans-imac.home>
2017-12-03 16:58:38 +0000

    move static markup to react

---
+++ __tests__/test-example-work.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork from '../js/example-work';
+
+describe("ExampleWork component", () => {
+
+  it("Should be a 'section' element", () => {
+    let component = shallow(<ExampleWork)
+  });
+});
